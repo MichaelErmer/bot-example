e371f350dfd1970a1e0d6de1356f415dec82d7e2
Michael Ermer <michael.ermer@ermer.de>
2017-03-16 22:08:54 +0100

    apply new scheme

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # Bot Example Plugin
 
 See code, its izi.
+Also Update Package.json
+
+## Usage
+
+    !install contactimpact/bot-example
+
+## Update
+
+    npm run publish
+
+*DON'T USE NPM PUBLISH! MIND THE **RUN***
+
+## Requirements
+
+_Add required packages to your package.json_
+
+### Environment
+
+_List required ENV variables here_
--- index.js
+++ index.js
@@ -1,19 +1,23 @@
+var package = require('./package.json');
 var _ = require('lodash');
 
 var Module = function (bot) {
   this.bot = bot;
-  this.name = "Example Library";
-  this.version = "0.1";
+  this.name = package.name;
+  this.version = package.version;
   this.help = function () {
     // RETURN HELP STRING FOR YOUR COMMANDS or AT LEAST YOUR COMMANDS Object.keys(this.commands)
-    return {
-      "example": "Says hello.",
-    };
+    // For basic help add them to the package
+    // This funciton can be used to generate a dynamic help, it is called at !help
+    return package.commands;
   };
   this.commands = {};
 
   this.commands.example = function(channel, args, user) {
     var response = "Hello, "+user.name;
+    // Do some async stuff
+    // remove Timeout for your module
+    // user is a user instance of slack, user.name is the "nice name"
     setTimeout(function() {
       bot.postMessage(channel, response);
     }, 500);
